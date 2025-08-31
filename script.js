51bc6acfa5df544359381af1638f3366ed0db478
beestcode <153935496+beestcode@users.noreply.github.com>
2025-08-31 14:50:03 +0500

    hero responsive done

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-    function toggleMenu() {
-        document.getElementById("menu").classList.toggle("show");
-        document.querySelector(".menu-btn").style.display = "none";
-    }
-
-    function closeMenu() {
-        document.getElementById("menu").classList.remove("show");
-        document.querySelector(".menu-btn").style.display = "block";
-    }
+    
+    //show and hide menu buton
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("show");
+    document.querySelector(".menu-btn").style.display = "none";
+}
+    //hide menu
+function closeMenu() {
+    document.getElementById("menu").classList.remove("show");
+    document.querySelector(".menu-btn").style.display = "block";
+}
